54563f6b71cc15dda1b3c0f29b80db6ce80c923f
Alena Sharai <exultantislupus@gmail.com>
2024-03-05 13:26:00 +0400

    feat: create Document Builder and Swagger Module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Music Home Library')
+    .setDescription('Music library for personal use')
+    .setVersion('1.0')
+    .addTag('music')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(PORT);
 }
 bootstrap();
