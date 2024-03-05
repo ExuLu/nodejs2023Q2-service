0578fb87f75f543407fc75f71682cdd4c15b6007
Alena Sharai <exultantislupus@gmail.com>
2024-03-05 15:44:32 +0400

    feat: add types for artists, tracks, favorites and albums, add them all to db type and to db

--- src/db/database.ts
+++ src/db/database.ts
@@ -2,4 +2,12 @@ import { Db } from 'src/types/database';
 
 export const database: Db = {
   users: [],
+  artists: [],
+  tracks: [],
+  albums: [],
+  favorites: {
+    artists: [],
+    tracks: [],
+    albums: [],
+  },
 };
--- src/types/database.ts
+++ src/types/database.ts
@@ -1,5 +1,13 @@
+import { Album } from './album';
+import { Artist } from './artist';
+import { Favorites } from './favorites';
+import { Track } from './track';
 import { User } from './user';
 
 export interface Db {
   users: User[];
+  artists: Artist[];
+  tracks: Track[];
+  albums: Album[];
+  favorites: Favorites;
 }
