bbd45e642cf3cd113e87bf87a20308c36b51b16b
Alena Sharai <exultantislupus@gmail.com>
2024-03-06 19:45:15 +0400

    feat: add artist controller and service to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,22 @@ import { UsersService } from './users/user.service';
 import { APP_PIPE } from '@nestjs/core';
 import { TrackController } from './tracks/track.controller';
 import { TrackService } from './tracks/track.service';
+import { ArtistService } from './artists/artist.service';
+import { ArtistController } from './artists/artist.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController, TrackController],
+  controllers: [
+    AppController,
+    UserController,
+    TrackController,
+    ArtistController,
+  ],
   providers: [
     AppService,
     UsersService,
     TrackService,
+    ArtistService,
     {
       provide: APP_PIPE,
       useValue: new ValidationPipe({
