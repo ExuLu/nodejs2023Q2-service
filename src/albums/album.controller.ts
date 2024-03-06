db8a117a8cc454cd845ba1d45cc836f0c430f9b3
Alena Sharai <exultantislupus@gmail.com>
2024-03-06 20:36:36 +0400

    feat: add getAllAlbums to controller and service

--- src/albums/album.controller.ts
+++ src/albums/album.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
 import { AlbumService } from './album.service';
+import { Album } from './albumType';
 
 @Controller('album')
 export class AlbumController {
   constructor(private readonly albumService: AlbumService) {}
+
+  @Get()
+  getAllAlbums(): Album[] {
+    return this.albumService.getAllAlbums();
+  }
 }
--- src/albums/album.service.ts
+++ src/albums/album.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Album } from './albumType';
+import { database } from 'src/db/database';
 
 @Injectable()
-export class AlbumService {}
+export class AlbumService {
+  getAllAlbums(): Album[] {
+    return database.albums;
+  }
+}
