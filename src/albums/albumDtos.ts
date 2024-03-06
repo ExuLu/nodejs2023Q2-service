fc66f5119d2d5f7b0a4505048922d3f9e5050632
Alena Sharai <exultantislupus@gmail.com>
2024-03-06 20:43:01 +0400

    feat: add AlbumCreate and AlbumUpdate dtos

--- src/albums/albumDtos.ts
+++ src/albums/albumDtos.ts
@@ -1,2 +1,30 @@
-export class AlbumCreateDto {}
-export class AlbumUpdateDto {}
+import { IsInt, IsNotEmpty, IsString, ValidateIf } from 'class-validator';
+
+export class AlbumCreateDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  year: number;
+
+  @ValidateIf((body) => body.artistId !== null)
+  @IsString()
+  @IsNotEmpty()
+  artistId: string | null;
+}
+export class AlbumUpdateDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  year: number;
+
+  @ValidateIf((body) => body.artistId !== null)
+  @IsString()
+  @IsNotEmpty()
+  artistId: string | null;
+}
