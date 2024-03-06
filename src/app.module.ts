396f3be3d973a5bf35687357b1c283c99e346314
Alena Sharai <exultantislupus@gmail.com>
2024-03-06 20:37:05 +0400

    feat: add album service and controller to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { TrackController } from './tracks/track.controller';
 import { TrackService } from './tracks/track.service';
 import { ArtistService } from './artists/artist.service';
 import { ArtistController } from './artists/artist.controller';
+import { AlbumController } from './albums/album.controller';
+import { AlbumService } from './albums/album.service';
 
 @Module({
   imports: [],
@@ -16,12 +18,14 @@ import { ArtistController } from './artists/artist.controller';
     UserController,
     TrackController,
     ArtistController,
+    AlbumController,
   ],
   providers: [
     AppService,
     UsersService,
     TrackService,
     ArtistService,
+    AlbumService,
     {
       provide: APP_PIPE,
       useValue: new ValidationPipe({
