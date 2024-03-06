40175fed90bdf29ba7d89d406594cb969c1d8521
Alena Sharai <exultantislupus@gmail.com>
2024-03-06 17:04:37 +0400

    refactor: add types to track controller

--- src/controllers/track.controller.ts
+++ src/controllers/track.controller.ts
@@ -9,6 +9,7 @@ import {
   HttpCode,
 } from '@nestjs/common';
 import { TrackService } from 'src/services/track.service';
+import { Track } from 'src/types/track';
 import { CreateTrackDto, UpdateTrackDto } from 'src/validators/trackValidators';
 
 @Controller('track')
@@ -16,28 +17,31 @@ export class TrackController {
   constructor(private readonly trackService: TrackService) {}
 
   @Get()
-  getAllTracks() {
+  getAllTracks(): Track[] {
     return this.trackService.getAllTracks();
   }
 
   @Get(':id')
-  getTrackById(@Param('id') id: string) {
+  getTrackById(@Param('id') id: string): Track {
     return this.trackService.getTrackById(id);
   }
 
   @Post()
-  addTrack(@Body() createTrackDto: CreateTrackDto) {
+  addTrack(@Body() createTrackDto: CreateTrackDto): Track {
     return this.trackService.addNewTrack(createTrackDto);
   }
 
   @Put(':id')
-  changeTrack(@Param('id') id: string, @Body() updateTrackDto: UpdateTrackDto) {
+  changeTrack(
+    @Param('id') id: string,
+    @Body() updateTrackDto: UpdateTrackDto,
+  ): Track {
     return this.trackService.changeTrack(id, updateTrackDto);
   }
 
   @Delete(':id')
   @HttpCode(204)
-  deleteTrack(@Param('id') id: string) {
+  deleteTrack(@Param('id') id: string): void {
     return this.trackService.deleteTrack(id);
   }
 }
