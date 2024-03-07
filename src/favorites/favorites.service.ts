2cb93437065851af773e6d90bbef857bc517e572
Alena Sharai <exultantislupus@gmail.com>
2024-03-07 13:43:21 +0400

    feat: add id validation

--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { database } from 'src/db/database';
 import { Favorites } from './favoritesType';
+import { validate } from 'uuid';
+import { NotValidIdException } from 'src/errors/notValidId';
 
 @Injectable()
 export class FavoriteService {
@@ -9,6 +11,9 @@ export class FavoriteService {
   }
 
   addTrackToFavs(id: string): object {
+    const idIsValid = validate(id);
+    if (!idIsValid) throw new NotValidIdException();
+
     const track = database.tracks.find((tr) => tr.id === id);
     database.favorites.tracks.push(track);
     return { message: 'Track was successfully added to favorites' };
