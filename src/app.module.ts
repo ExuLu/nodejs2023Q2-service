6686258a41f2d12a386c319e30a35bec2be4a8b6
Alena Sharai <exultantislupus@gmail.com>
2024-03-07 18:48:29 +0400

    feat: create track module, add service and controller there and delete them from app module, instead add track module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_PIPE } from '@nestjs/core';
-import { TrackController } from './tracks/track.controller';
-import { TrackService } from './tracks/track.service';
 import { ArtistService } from './artists/artist.service';
 import { ArtistController } from './artists/artist.controller';
 import { AlbumController } from './albums/album.controller';
@@ -11,19 +9,18 @@ import { AlbumService } from './albums/album.service';
 import { FavoritesController } from './favorites/favorites.controller';
 import { FavoriteService } from './favorites/favorites.service';
 import { UserModule } from './users/user.module';
+import { TrackModule } from './tracks/track.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, TrackModule],
   controllers: [
     AppController,
-    TrackController,
     ArtistController,
     AlbumController,
     FavoritesController,
   ],
   providers: [
     AppService,
-    TrackService,
     ArtistService,
     AlbumService,
     FavoriteService,
