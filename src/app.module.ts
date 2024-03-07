2e74516b0e2c11925960cfec9e104f21ea98c702
Alena Sharai <exultantislupus@gmail.com>
2024-03-07 19:00:38 +0400

    refactor: add album module to app module and delete album controller and service from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,23 @@ import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_PIPE } from '@nestjs/core';
-import { AlbumController } from './albums/album.controller';
-import { AlbumService } from './albums/album.service';
 import { UserModule } from './users/user.module';
 import { TrackModule } from './tracks/track.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { ArtistModule } from './artists/artist.module';
+import { AlbumModule } from './albums/album.module';
 
 @Module({
-  imports: [UserModule, TrackModule, FavoritesModule, ArtistModule],
-  controllers: [AppController, AlbumController],
+  imports: [
+    UserModule,
+    TrackModule,
+    FavoritesModule,
+    ArtistModule,
+    AlbumModule,
+  ],
+  controllers: [AppController],
   providers: [
     AppService,
-    AlbumService,
     {
       provide: APP_PIPE,
       useValue: new ValidationPipe({
